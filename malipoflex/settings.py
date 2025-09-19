352a172a557dc5bcb3253361ea66f7ec4329d66c
student <oumagladwineesther@gmail.com>
2025-09-19 11:59:17 +0300

    Added corheaders in settings.py

--- malipoflex/settings.py
+++ malipoflex/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'corsheaders'
+    'corsheaders',
     'loans',    
     'api',  
     'rest_framework.authtoken',
